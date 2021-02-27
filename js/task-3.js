5ff1e3fd0bddfab8e00529d5cd6f8a05bd1ad3f4
Dmytro Filius <mindertan@gmil.com>
2021-02-27 16:14:27 +0200

    update task - 3

--- js/task-3.js
+++ js/task-3.js
@@ -17,51 +17,10 @@ const images = [
 ];
 
 const galleryRef = document.querySelector('#gallery')
-// const imageList = document.createElement('li')
-// const image = document.createElement('img')
-// image.classList.add('image')
-// const imageList = images.map(image => {
-//   const imageEl = document.createElement('img')
-//   imageEl.classList.add('image')
-//   return galleryRef.insertAdjacentHTML('beforeend', `src = ${ image.url }`)
-//   // return imageEl
-// })
-
-
 const imageList = (image) => {
   return image.map(image => {
-    const imageEl = document.createElement('img')
-    return galleryRef.insertAdjacentHTML('beforeend', `<li><img class = "image" src = ${image.url}></img></li>`)
+    return galleryRef.insertAdjacentHTML('beforeend', `<li><img class = "image" src = ${image.url} alt = '${image.alt}'></img></li>`)
   })
 }
 const element = imageList(images)
-galleryRef.append(...element)
-// galleryRef.appendChild(imageList)
-// imageList.appendChild(image)
-// console.log(galleryRef);
-// console.log(imageList);
-// const image = (image) => {
-//   const imageList = document.createElement('li img')
-//   imageList.classList.add('image')
-//   imageList.insertAdjacentHTML('afterbegin') = image;
-//   return imageList;
-// }
-// const renderItem = (list) => {
-//   const renderList = list.map((url, alt) => `<img src ="${url}" alt='${alt}'`)
-//   renderList.append(...imageList)
-// }
-// renderList(images)
-
-
-// const image = document.createElement('img')
-// image.classList.add('image')
-// image.setAttribute('src', 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
-// image.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-
-// galleryRef.appendChild(image)
-
-// const imageList = images
-//   .map(({ url, alt }) => `<img src ="${url}" alt='${alt}'`)
-//   .join(' ')
-// console.log(imageList);
-// galleryRef.append(...imageList)
+console.log(galleryRef);
